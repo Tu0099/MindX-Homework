ea169fbfda42cb04ce03030c377c26eb59e62547
ngoctuzero@gmail.com <havin.stu09@gmail.com>
2020-03-20 01:00:43 +0700

    bt1

---
+++ app.js
@@ -0,0 +1,32 @@
+// tính diện tích hình tròn
+
+const input = Number(prompt("Radius"));
+
+const  diameter = input * 2;
+
+const s = diameter * 3.14;
+
+const name = "Area = ";
+
+const totalString = `${name} ${S}`;
+
+console.log(totalString);
+
+
+
+
+// chuyển đổi Celsius (0C) thành Fahrenheit (0F)
+
+const Celsius = Number(prompt("Celsius"));
+
+const Fahrenheit = (Celsius * 1.8) + 32;
+
+const name = "Fahrenheit =";
+
+const totalString = `${name} ${Celsius}`;
+
+console.log(totalString);
+
+
+
+
